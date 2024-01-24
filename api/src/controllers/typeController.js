833e87b5d18fd01cf3c9bc176b94b6ac30e1c2bd
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-01-24 12:12:04 -0300

    Logré que el controlador de types me traida los types :)

--- api/src/controllers/typeController.js
+++ api/src/controllers/typeController.js
@@ -1,9 +1,18 @@
 const { Type } = require('../db');
-
+const axios = require('axios');
 
 const getTypePokemon = async() => {
-    const typePokemonsDB = await Type.findAll();
-    return typePokemonsDB;
+    const allTypes = [];
+
+    const infoApi = await axios.get('https://pokeapi.co/api/v2/type')
+    const resultApi = await infoApi.data.results;
+    //console.log(resultApi)
+    resultApi.map((type) => allTypes.push(type.name))
+    //console.log(allTypes)
+    await Promise.all(allTypes.map((type) => {Type.findOrCreate({ where: {name: type}} )}
+    ));
+    const infoDB = await Type.findAll();
+    return infoDB;
 };
 
 module.exports = {
