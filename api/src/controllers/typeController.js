e43d7d461f3b5e06dec0fdbb785400c9eb566622
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-01-25 06:25:20 -0300

    Modifiqué typescontrolers por que me devolvía un objeto y necesitaba un array

--- api/src/controllers/typeController.js
+++ api/src/controllers/typeController.js
@@ -2,17 +2,20 @@ const { Type } = require('../db');
 const axios = require('axios');
 
 const getTypePokemon = async() => {
-    const allTypes = [];
-
-    const infoApi = await axios.get('https://pokeapi.co/api/v2/type')
-    const resultApi = await infoApi.data.results;
-    //console.log(resultApi)
-    resultApi.map((type) => allTypes.push(type.name))
-    //console.log(allTypes)
-    await Promise.all(allTypes.map((type) => {Type.findOrCreate({ where: {name: type}} )}
-    ));
-    const infoDB = await Type.findAll();
-    return infoDB;
+    const typesDB = await Type.findAll();
+    if(!typesDB.length) {
+        const infoApi = await axios.get('https://pokeapi.co/api/v2/type')
+        const resultApi = await infoApi.data.results;
+        //console.log(resultApi)
+        let types = [];
+        resultApi.forEach((type) => types.push(type.name))
+        let typesUnicos = new Set(types.flat())//new set saca los repetidos pero devuelve un objeto
+        let typeArray = [...typesUnicos];//entonces con el spread operator lo paso a array
+        typeArray.forEach(async(type) => {await Type.findOrCreate({where: {name: type}})
+    })        
+    return typeArray;
+    }
+    return typesDB.map((t) => t.name)//para que vuelva a ser array
 };
 
 module.exports = {
