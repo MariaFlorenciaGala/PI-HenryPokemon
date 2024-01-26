02730bdfc2778a78155ada59d3496e9984f214ab
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-01-26 18:43:29 -0300

    Trabajé en las routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
-
-import {BrowseRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Views/Home/Home'
+import Detail from './Views/Detail/Detail'
+import Create from './Views/Create/Create'
+import Landing from './Views/Landing/Landing'
+import Nav from './Components/Nav/Nav'
     
 
 function App() {
   return (
     <div className='App'>
-      <h1>POKEMONES</h1>
-      <BrowseRouter>
+      <BrowserRouter>
+      <Routes>
+          <Route path={'*'} element={<Nav />}/>
+      </Routes>
         <Routes>
-          <Route path={'/'} component={Landing}/>
+          <Route exact path={'/'} element={<Landing />}/>
+          <Route path={'/home'} element={<Home />}/>
+          <Route path={'/detail:id'} element={<Detail />}/>
+          <Route path={'/create'} element={<Create />}/>
         </Routes> 
-      </BrowseRouter>
+      </BrowserRouter>
     </div>
   )
 }
